193d63874c45cc5a44707d86b15759adc82447b7
Hiro Lestrade <hiroshi.lestrade@ingenieria.unam.edu>
2023-07-10 23:21:50 -0600

    fix: new_account error

--- wallet_cli.py
+++ wallet_cli.py
@@ -5,19 +5,22 @@ import click
 def wallet_cli():
     pass
 
-@cli.command()
-@click.option('--account', requred=True, help='Name account')
-@click.option('--type', required=True, help='Cash, credit, debit or departamental')
-@click.option('--amount', require=True, help='Available money')
-@clicl.option('--limit', help='Limit in case of credit or departamental')
+@wallet_cli.command()
+@click.option('--account', required=True, help='Name account')
+@click.option('--atype', required=True, help='Account type: cash, credit, debit or departamental')
+@click.option('--amount', required=True, help='Available money')
+@click.option('--limit', help='Limit in case of credit or departamental')
 @click.pass_context
 def new_account(ctx, account, atype, amount, limit):
     if not account or not atype or not amount:
-        ctx.fail('Account, type and amount are required')
+        ctx.fail('Account, atype and amount are required')
     else:
         data = json_manager.read_json()
-        new_id = len(data) + 1
+        new_id = len(data['accounts']) + 1
         new_reg = {'id':new_id, 'account':account, 'type':atype, 'amount': amount, 'limit':limit}
         data['accounts'].append(new_reg)
         json_manager.write_json(data)
         print(f"new account created with id {new_id}")
+
+if __name__ == '__main__':
+    wallet_cli()
